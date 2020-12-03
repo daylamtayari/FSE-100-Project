88c06a0165c177ce697673db2103b503890371a0
Daylam Tayari <58621925+daylamtayari@users.noreply.github.com>
2020-12-03 14:33:19 -0700

    Syntax fixes
    Syntax fixes and polishing up of the entire code.

--- Exercise2.js
+++ Exercise2.js
@@ -10,21 +10,16 @@ let onTrack = false; //Keeps track of user's input on the ellipse
 let timer = 0; //Timer to track how long user is following ellipse
 
 
-
 function setup() {
   createCanvas(1280, 720);
   ellipseMode(RADIUS); //Sets ellipse values to radius
   //Starting position for ellipse
   x = width / 2;
   y = height / 2;
-  
-
 }
 
 function draw() {
-
   background(220);
-  
 
   //Updating coordinates of the ellipse
   x = x + xSpeed * xDirection;
@@ -52,34 +47,27 @@ function draw() {
 }
 
 //Function that collects how many times ellipse has bounced and whether or not to increase the speed
-function bounceCount() 
-{
+function bounceCount() {
   if ((bounce % 5) == 0) {
     //Speeds up ball on every bounce multiple of 5
     xSpeed = 5;
     ySpeed = 5;
-    
   }
-  else
-  {
-  //Returns normal speed
-  xSpeed = 3;
-  ySpeed = 3;
+  else{
+    //Returns normal speed
+    xSpeed = 3;
+    ySpeed = 3;
   }
 }
 
 //Changes color of the ellipse when mouse is on it
-function userTrack()
-{
-  
-  if(dist (x,y,mouseX,mouseY) < size )
-  {
+function userTrack(){
+  if(dist (x,y,mouseX,mouseY) < size ){
     fill('green');
     ellipse(x, y, size, size);
     onTrack = true;
   }
-  else 
-  {
+  else {
     fill('yellow');
     ellipse(x,y, size, size);
     onTrack = false;
@@ -87,19 +75,16 @@ function userTrack()
 }
 
 //Prints on screen instructions + time
-function printText()
-{
+function printText(){
   //Text
   fill('black');
   textSize(40);
   text('Follow the ball!', 480, 100);
   
-  if(frameCount % 60 == 0 && onTrack == true)
+  if(frameCount % 60 == 0 && onTrack == true){
     timer++;
-  
+  }
   textSize(40);
   fill('black');
   text('Followed the ball for ' + timer + ' seconds!', 350, 600);
-  
 }
-
